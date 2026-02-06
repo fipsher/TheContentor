1b9b292b45edd072f6ec3301fb5673bddb0f6bfe
azvarych <andrii.zvarych@ryan.com>
2026-02-06 12:49:55 +0100

    feat(migrations): Add YouTube asset properties to the Assets table
    - Added database columns: `Height`, `Width`, `OriginalUrl`, `Title`, `Type`, and `UploadDate` to support YouTube asset metadata.
    - Implemented migration to update the `Assets` table schema.

--- src/Infrastructure/TheContentor.Infrastructure/Migrations/20260206113558_AddYouTubeAssetProperties.Designer.cs
+++ src/Infrastructure/TheContentor.Infrastructure/Migrations/20260206113558_AddYouTubeAssetProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TheContentor.Infrastructure;
@@ -12,9 +13,11 @@ using TheContentor.Infrastructure;
 namespace TheContentor.Infrastructure.Migrations
 {
     [DbContext(typeof(TheContentorDbContext))]
-    partial class TheContentorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260206113558_AddYouTubeAssetProperties")]
+    partial class AddYouTubeAssetProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
